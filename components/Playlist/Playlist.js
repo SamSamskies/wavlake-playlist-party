f03739c8a7d5819a71fb6afe4834e073b9103280
Sam Samskies <samsamskies@gmail.com>
2024-07-31 12:20:40 -0500

    MVP 🎉

--- components/Playlist/Playlist.js
+++ components/Playlist/Playlist.js
@@ -4,7 +4,11 @@ import { QRCodeSVG } from "qrcode.react";
 import { bech32 } from "bech32";
 import { utf8ToBytes } from "@noble/hashes/utils";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Image from "next/image";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { Logo } from "./Logo";
 
 const poppins = Poppins({ weight: ["400", "700"], subsets: ["latin"] });
 
@@ -22,50 +26,115 @@ const fetchPlaylist = (playlistId) =>
     .then((res) => res.json())
     .then((res) => res.data);
 
+const fetchTrackBackgroundImage = (trackId) =>
+  fetch(
+    `${process.env.NEXT_PUBLIC_WAVLAKE_CATALOG_API_BASE_URL}/v1/tracks/${trackId}`,
+  )
+    .then((res) => res.json())
+    .then((res) => res.data.avatarUrl);
+
 export const Playlist = ({ playlistId }) => {
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
-  const { data } = useQuery({
-    queryKey: ["playlist", playlistId],
+  const { data: playlist } = useQuery({
+    queryKey: ["playlists", playlistId],
     queryFn: () => fetchPlaylist(playlistId),
+    staleTime: Infinity,
   });
-  const currentTrack = data?.tracks ? data.tracks[currentTrackIndex] : null;
+  const currentTrack = playlist?.tracks
+    ? playlist.tracks[currentTrackIndex]
+    : null;
+  const { data: currentTrackBackgrounImageSrc } = useQuery({
+    queryKey: ["trackBackgroundImages", currentTrack?.id],
+    queryFn: () => fetchTrackBackgroundImage(currentTrack?.id),
+    staleTime: Infinity,
+    enabled: Boolean(currentTrack?.id),
+  });
+  const playerRef = useRef(null);
+  const playNext = () => {
+    setCurrentTrackIndex((currentTrack) =>
+      currentTrack < playlist.tracks.length - 1 ? currentTrack + 1 : 0,
+    );
+  };
+  const playPrevious = () => {
+    if (playerRef.current && currentTrackIndex === 0) {
+      const audio = playerRef.current.audio.current;
+
+      audio.currentTime = 0;
+      audio.play();
+
+      return;
+    }
+
+    setCurrentTrackIndex((currentTrack) => currentTrack - 1);
+  };
+  console.log(playerRef.current);
 
   return (
     <main className={`${poppins.className} ${styles.main}`}>
-      <h1>WAVLAKE</h1>
-      {currentTrack && (
-        <>
-          <h2>{data.title}</h2>
-          <div className={styles.container}>
-            <div>
-              <img
-                src={currentTrack.artworkUrl}
-                alt={`artwork for ${currentTrack.title}`}
-                className={styles.responsiveSquares}
-              />
-              <div>
-                <p className={styles.boldText}>{currentTrack.title}</p>
-                <p>{currentTrack.artist}</p>
-              </div>
-            </div>
-            <div>
-              <QRCodeSVG
-                value={`lightning:${encodeTrackLnurl(currentTrack.id)}`}
-                includeMargin
-                size={500}
-                className={styles.responsiveSquares}
-              />
-              <div>
-                <p className={styles.boldText}>Boost and support this artist</p>
-                <p>
-                  Scan this QR code from any lightning wallet to send sats
-                  directly to the artist.
-                </p>
-              </div>
-            </div>
-          </div>
-        </>
+      {currentTrackBackgrounImageSrc && (
+        <Image
+          src={currentTrackBackgrounImageSrc}
+          alt="Track background image"
+          fill
+          objectFit="cover"
+        />
       )}
+      <div className={styles.container}>
+        {currentTrack && (
+          <>
+            <div className={styles.topLeftCorner}>
+              <Logo style={{ flexBasis: 108 }} />
+              <div>
+                <p>PLAYING FROM PLAYLIST</p>
+                <p className={styles.boldText}>{playlist.title}</p>
+              </div>
+            </div>
+            <div className={styles.bottomLeftCorner}>
+              <div>
+                <Image
+                  src={currentTrack.artworkUrl}
+                  alt={`artwork for ${currentTrack.title}`}
+                  width={400}
+                  height={400}
+                />
+                <div>
+                  <p className={styles.boldText}>{currentTrack.title}</p>
+                  <p>{currentTrack.artist}</p>
+                </div>
+              </div>
+              <div>
+                <QRCodeSVG
+                  value={`lightning:${encodeTrackLnurl(currentTrack.id)}`}
+                  includeMargin
+                  size={400}
+                  className={styles.responsiveSquares}
+                />
+                <div>
+                  <p className={styles.boldText}>
+                    Boost and support this artist
+                  </p>
+                  <p>
+                    Scan this QR code from any lightning wallet to send sats
+                    directly to the artist.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.audioControlsContainer}>
+              <AudioPlayer
+                ref={playerRef}
+                autoPlay
+                src={currentTrack.liveUrl}
+                showSkipControls
+                onClickPrevious={playPrevious}
+                onClickNext={playNext}
+                onEnded={playNext}
+                style={{ backgroundColor: "transparent", boxShadow: "none" }}
+              />
+            </div>
+          </>
+        )}
+      </div>
     </main>
   );
 };
