c2f6f076d22ec3fc253345714860235f9ac743cb
Sam Samskies <samsamskies@gmail.com>
2024-10-05 17:05:48 -0500

    fix og base URL for prod

--- utils/getBaseUrl.js
+++ utils/getBaseUrl.js
@@ -1,4 +1,8 @@
 export function getBaseUrl() {
+  if (process.env.NEXT_PUBLIC_SITE_URL) {
+    return process.env.NEXT_PUBLIC_SITE_URL;
+  }
+
   if (process.env.NEXT_PUBLIC_VERCEL_URL) {
     return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
