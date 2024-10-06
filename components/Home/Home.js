05afc86300e581700c36caf62e30bd1b29b549b0
Sam Samskies <samsamskies@gmail.com>
2024-10-06 15:08:46 -0500

    display playlists for npub

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,8 +1,18 @@
 import styles from "./Home.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { getBaseUrl } from "@/utils/getBaseUrl";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+
+const getLibraryPlaylists = async (pubkey) => {
+  return fetch(
+    `${process.env.NEXT_PUBLIC_WAVLAKE_CATALOG_API_BASE_URL}/v1/playlists/user/${pubkey}`,
+  )
+    .then((res) => res.json())
+    .then((res) => res.data);
+};
 
 function extractTrackIdFromWavlakeUrl(url) {
   const regex =
@@ -14,7 +24,14 @@ function extractTrackIdFromWavlakeUrl(url) {
 
 export const Home = () => {
   const router = useRouter();
-  const [error, setError] = useState("");
+  const [pubkey, setPubkey] = useState(null);
+  const { data: playlists = [] } = useQuery({
+    queryKey: ["playlists", pubkey],
+    queryFn: () => getLibraryPlaylists(pubkey),
+    enabled: Boolean(pubkey),
+    staleTime: Infinity,
+  });
+  const [error, setError] = useState(null);
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
@@ -30,6 +47,35 @@ export const Home = () => {
   };
   const baseUrl = getBaseUrl();
   const ogImage = `${baseUrl}/api/og`;
+
+  useEffect(() => {
+    const handleAuthEvent = (event) => {
+      if (event.detail.type === "login") {
+        setPubkey(event.detail.pubkey);
+      }
+
+      if (event.detail.type === "logout") {
+        setPubkey(null);
+      }
+    };
+
+    document.addEventListener("nlAuth", handleAuthEvent);
+
+    return () => {
+      document.removeEventListener("nlAuth", handleAuthEvent);
+    };
+  }, []);
+
+  useEffect(() => {
+    import("nostr-login")
+      .then(({ init }) =>
+        init({
+          darkMode: true,
+          methods: ["extension", "readOnly"],
+        }),
+      )
+      .catch(console.error);
+  }, []);
 
   return (
     <>
@@ -51,6 +97,33 @@ export const Home = () => {
           </button>
         </form>
         {error && <p className={styles.error}>{error}</p>}
+        <div className={styles.playlistsContainer}>
+          {playlists.map(({ id, title: playlistTitle, tracks }) => (
+            <div
+              key={id}
+              className={styles.playlistRow}
+              onClick={() => router.push(`/playlists/${id}`)}
+            >
+              <div className={styles.playlistArt}>
+                {tracks
+                  .slice(0, 4)
+                  .map(({ id, artworkUrl, title: trackTitle }) => (
+                    <Image
+                      key={id}
+                      src={artworkUrl}
+                      alt={`${trackTitle} art`}
+                      width={500}
+                      height={500}
+                    />
+                  ))}
+              </div>
+              <div className={styles.playlistMetadata}>
+                <p>{playlistTitle}</p>
+                <p>{tracks.length} tracks</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
